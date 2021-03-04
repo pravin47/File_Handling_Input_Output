719888af987da86a4da0b54f5a89fd1d3cd1c986
pravin bagul <bagul.pravin2@gmail.com>
2021-03-04 22:30:27 +0530

    Added:Write_Employee_Payroll_To_Console

--- File_Handling.java
+++ File_Handling.java
@@ -1,10 +1,67 @@
 package com.employee.payroll;
 
+import java.util.Scanner;
+
+import java.util.LinkedList;
+
 public class EmployeePayrollService {
 
 	public static void main(String args[]) {
+		LinkedList<EmployeeData> Store = new LinkedList<EmployeeData>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The Employee Id");
+		int id;
+		id = sc.nextInt();
+		System.out.println("Enter The Employee Name");
+		String name;
+		name = sc.next();
+		System.out.println("Enter The Employee Salary");
+		Double salary = sc.nextDouble();
+		EmployeeData obj = new EmployeeData(id, name, salary);
+		Store.add(obj);
+		System.out.println(Store);
+	}
+}
 
-		System.out.println("Welcome To File Handling Programmin");
+class EmployeeData {
+	private int id;
+	private String name;
+	private Double salary;
+
+	public String toString() {
+
+		return "EmployeeData[ID:" + id + " NAME :" + name + " SALARY:" + salary + "]";
 
 	}
+
+	EmployeeData(int id, String name, Double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
 }

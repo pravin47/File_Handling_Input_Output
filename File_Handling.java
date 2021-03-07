e7c764dd0ef5a2fa87322cfcd009da9b1bd5a24d
pravin bagul <bagul.pravin2@gmail.com>
2021-03-07 14:29:45 +0530

    Demonstrate_The_File_Operration

--- File_Handling.java
+++ File_Handling.java
@@ -1,67 +1,112 @@
 package com.employee.payroll;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
-import java.util.LinkedList;
-
 public class EmployeePayrollService {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void addNewFile() {
+		try {
+			System.out.println("Enter The New Directory Path");
+			String path = sc.next();
+			File Dir = new File(path);
+			if (Dir.exists()) {
+				System.out.println("Directory All Ready Present");
+			} else {
+				System.out.println("Its A New Directory Its Creating now...");
+			}
+			Dir.mkdir();
+			System.out.println("Path has Created...");
+			System.out.println("Enter The File Name You Want To Create");
+			String file = sc.next();
+			File file1 = new File(path, file);
+			if (file1.exists()) {
+
+				System.out.println("This File AllReady Present");
+			} else {
+				System.out.println("Its New File Its Creating...");
+				file1.createNewFile();
+				System.out.println("New File Has Been Created");
+			}
+
+		} catch (Exception e) {
+			System.out.println("An Error Occured");
+			e.printStackTrace();
+		}
+	}
+
+	public static void deleteFile() {
+		try {
+			System.out.println("Enter The Path You Want To delete The File...");
+			String path = sc.next();
+			File Dir = new File(path);
+			if (Dir.exists()) {
+				System.out.println("Enter The File You Want To Delete");
+			} else {
+				System.out.println("Path Does Not Exist");
+			}
+			System.out.println("Enter The File Name You Want To Delete...");
+			String file = sc.next();
+			File File1 = new File(Dir, file);
+			if (File1.delete()) {
+
+				System.out.println("File Has Been Successfully Deleted....");
+			} else {
+				System.out.println("File Does Not Exist...");
+			}
+
+		} catch (Exception e) {
+			System.out.println("An Error Occured...");
+			e.printStackTrace();
+		}
+	}
+
+	public static void listFile() {
+		try {
+			System.out.println("Enter The Path You Want To Lis There Files");
+			String path = sc.next();
+			File Dir = new File(path);
+			String[] list1 = Dir.list();
+			for (String list2 : list1) {
+				System.out.println(list2);
+
+			}
+
+		} catch (NullPointerException e) {
+			System.out.println("An Error Occured...");
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String args[]) {
-		LinkedList<EmployeeData> Store = new LinkedList<EmployeeData>();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter The Employee Id");
-		int id;
-		id = sc.nextInt();
-		System.out.println("Enter The Employee Name");
-		String name;
-		name = sc.next();
-		System.out.println("Enter The Employee Salary");
-		Double salary = sc.nextDouble();
-		EmployeeData obj = new EmployeeData(id, name, salary);
-		Store.add(obj);
-		System.out.println(Store);
+
+		System.out.println("WelCome To File Handling Programm");
+		System.out.println("Enter The Your Choice");
+		boolean ans = true;
+		while (ans != false) {
+			System.out.println("1 For Creating New File");
+			System.out.println("2 For Deleteing File");
+			System.out.println("3 List The File");
+			System.out.println("4 For Exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+
+			case 1:
+				addNewFile();
+				break;
+			case 2:
+				deleteFile();
+				break;
+			case 3:
+				listFile();
+				break;
+			case 4:
+				ans = false;
+				break;
+
+			}
+		}
 	}
-}
-
-class EmployeeData {
-	private int id;
-	private String name;
-	private Double salary;
-
-	public String toString() {
-
-		return "EmployeeData[ID:" + id + " NAME :" + name + " SALARY:" + salary + "]";
-
-	}
-
-	EmployeeData(int id, String name, Double salary) {
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
-
 }
